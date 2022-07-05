f6f601669e4a4c87a7c40f47d7a1946cef1af57c
Nikolay <nick.chuchunov@yandex.ru>
2022-07-05 22:25:57 +0300

    тесте GitKraken

---
+++ MessageDriven ArchitectureCNN_1/MessageDriven ArchitectureCNN_1/MessageDriven ArchitectureCNN_1/Task4/SAGA/GitKrakenTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageDriven_ArchitectureCNN_1.Task4.SAGA
+{
+    internal class GitKrakenTest
+    {
+        //Класс создан в целях теста
+
+    }
+}
